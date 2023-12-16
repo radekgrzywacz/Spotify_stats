c23b2961fd7b901927f9835e8a52dd6834564224
radox9 <grzywaczra@gmail.com>
2023-12-16 11:50:25 +0100

    Main file

--- Projekt/PK3_project/PK3_project/Program.cs
+++ Projekt/PK3_project/PK3_project/Program.cs
@@ -1,6 +1,18 @@
+using PK3_project.CsvWork;
+
 namespace PK3_project;
 
 public class Program
 {
-    
+    public static void Main(string[] args)
+    {
+
+        const string filepath =
+            "/Users/radek/RiderProjects/4c668c11-gr03-repo/Projekt/PK3_project/PK3_project/spotify_artist_data.csv";
+        
+        
+        var reader = new Reader();
+        var results = reader.read(filepath).ToList();
+
+    }
 }
